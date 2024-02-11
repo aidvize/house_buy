ad11ac9f2ab09b01f684da09e8fac703a1c638a8
whoismurilo <murilobarbosa1202@gmail.com>
2024-02-11 01:44:56 +0000

    [fix] - transform in function

--- src/data/imovirtual.py
+++ src/data/imovirtual.py
@@ -1,11 +1,21 @@
-from src.utils.utils import save_to_json, scrape_imovirtual_listings
+from src.utils.utils import (
+    load_config,
+    performance_metrics,
+    save_to_json,
+    scrape_imovirtual_listings,
+)
 
-if __name__ == "__main__":
-    base_url = "https://www.imovirtual.com/comprar/apartamento"
-    titles, links = scrape_imovirtual_listings(base_url, start_page=1, max_pages=10)
 
-    # Convert titles and links into a dictionary
+@performance_metrics
+def imovirtual():
+    config = load_config()
+
+    base_url = config["imovirtual"]["base_url"]
+    max_pages = config["imovirtual"]["max_pages"]
+
+    titles, links = scrape_imovirtual_listings(base_url, start_page=1, max_pages=max_pages)
     data = {title: link for title, link in zip(titles, links)}
-
-    # Save the data to the JSON file
     save_to_json(data)
+
+
+imovirtual()
